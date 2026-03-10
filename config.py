730c3ffe9eb4a64d889a7049176891502abe639f
CHESTER <115614081+cakosik@users.noreply.github.com>
2026-03-11 02:41:37 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-BOT_TOKEN='5102065471:AAGTm07r-xN-xuWeQPgD0b3mf0XKUaGfvA8'
+BOT_TOKEN='8635962132:AAFVR_2j3WwEAITaDmGtnQZUpCvlyASzrxg'
 # test=
 # orig=
-owner_id=5045853241
+owner_id=6194786755
 group_newbot=-751327661
 Test_group=-1001835471301
 offical_group= -1001931243692
@@ -11,5 +11,6 @@ report_group=-1001933234957
 BOT_NICKNAME='varzay_bot'
 CHAT = '<a href="t.me/varzay_game_chat">Varzay | Игровой чат #1</a>'
 QIWI_TOKEN='eyJ2ZXJzaW9uIjoiUDJQIiwiZGF0YSI6eyJwYXlpbl9tZXJjaGFudF9zaXRlX3VpZCI6IjFyc3QweS0wMCIsInVzZXJfaWQiOiI5OTY3MDU0NDkxMTEiLCJzZWNyZXQiOiIzMDMyNGQwMGEyMDgwOTA5NDlhMTk2OTIxNDJkNThlMGVkMmM5MDZhMGQ1MTVkYzIzOTliMTVhNGFiN2Y5OGE0In19'
+
 
 vixod = '1.10.2023'
